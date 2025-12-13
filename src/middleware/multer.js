180b321789a665b7da6e2c200935230b23b6bd7b
Khubaib Jimoh <khubaibjimoh@gmail.com>
2025-12-13 23:17:27 +0100

    Setup authentication and user profile endpoints; WIP avatar upload/delete

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -6,7 +6,7 @@ import cloudinary from "../config/cloudinary.js";
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "portify/avatars",
+    folder: "upload/avatars",
     allowed_formats: ["jpg", "jpeg", "png", "webp"],
     transformation: [{ width: 400, height: 400, crop: "fill" }],
   },
